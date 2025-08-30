6b137dd9f879a381a11d83400d45c1771b1671eb
hsurekaMD <40699662+hsurekaMD@users.noreply.github.com>
2025-08-30 22:00:50 +0530

    commit payment success flow

---
+++ TaxFiling.Business/Repositories/UserTransactionsRepository.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxFiling.Business.Interfaces;
+using TaxFiling.Data;
+using TaxFiling.Domain.Dtos;
+using TaxFiling.Domain.Entities;
+
+
+namespace TaxFiling.Business.Repositories
+{
+    public class UserTransactionsRepository : IUserTransactionsRepository
+    {
+        private readonly Context _context;
+        private readonly ILogger<PackagesRepository> _logger;
+        private readonly IConfiguration _configuration;
+
+        public UserTransactionsRepository(Context context, ILogger<PackagesRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Save initial UserTransaction and return the generated OrderId
+        /// </summary>
+        public async Task<int> SaveUserTransaction(UserTransactions transaction)
+        {
+            if (transaction == null)
+                throw new ArgumentNullException(nameof(transaction));
+
+            try
+            {
+                // Set creation date if not already set
+                if (!transaction.TransactionCreatedDate.HasValue)
+                    transaction.TransactionCreatedDate = DateTime.UtcNow;
+
+                // Only initial columns
+                var entity = new UserTransactions
+                {
+                    //OrderNumber = transaction.OrderNumber,
+                    UserId = transaction.UserId,
+                    PackageId = transaction.PackageId,
+                    TransactionAmount = transaction.TransactionAmount,
+                    TransactionStatus = transaction.TransactionStatus,
+                    Currency = transaction.Currency,
+                    TransactionCreatedDate = transaction.TransactionCreatedDate
+                };
+
+                await _context.UserTransactions.AddAsync(entity);
+                await _context.SaveChangesAsync();
+
+                _logger.LogInformation("Initial UserTransaction saved with OrderId: {OrderId}", entity.OrderId);
+
+                entity.OrderNumber = $"ORDTAX-{entity.OrderId:0000}";
+                _context.UserTransactions.Update(entity);
+                await _context.SaveChangesAsync();
+
+                _logger.LogInformation("UserTransaction saved and OrderNumber updated: {OrderNumber}", entity.OrderNumber);
+
+                // Return the generated OrderId
+                return entity.OrderId;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error saving initial UserTransaction");
+                throw;
+            }
+        }
+
+        public async Task UpdatePaymentDetailsAsync(int orderId, string onePayTransactionId, bool onePayStatus, string onePayPaidOn, string onePayRequestedDate, int PackageId)
+        {
+            var transaction = await _context.UserTransactions
+                .Include(t => t.User)  // make sure navigation property exists
+                .FirstOrDefaultAsync(t => t.OrderId == orderId);
+
+            if (transaction == null)
+                throw new Exception("Transaction not found");
+
+            transaction.OnePayTransactionId = onePayTransactionId;
+            transaction.OnePayStatus = onePayStatus;
+            transaction.OnePayPaidOn = onePayPaidOn;
+            transaction.OnePayTransactionRequestDatetime = onePayRequestedDate;
+            if(onePayStatus && onePayPaidOn != null)
+                transaction.TransactionStatus = 1;
+            else
+                transaction.TransactionStatus = 0;
+
+            // Update user's is_active_payment if payment succeeded
+            if (onePayStatus && transaction.User != null)
+            {
+                transaction.User.IsActivePayment = 1;
+                transaction.User.PackageId = PackageId;
+            }
+            //_context.UserTransactions.Update(transaction);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<UserTransactionViewDto?> GetTransactionByIdAsync(int orderId)
+        {
+            var transaction = await _context.UserTransactions
+               .Include(t => t.User)
+               .Include(t => t.Package)
+               .Where(t => t.OrderId == orderId)
+               .Select(t => new UserTransactionViewDto
+               {
+                   OrderId = t.OrderId,
+                   OrderNumber = t.OrderNumber,
+                   UserFullName = $"{t.User.FirstName} {t.User.LastName}",
+                   PackageName = t.Package.Name,
+                   TransactionAmount = t.TransactionAmount,
+                   Currency = t.Currency,
+                   TransactionCreatedDate = t.TransactionCreatedDate,
+                   OnePayTransactionId = t.OnePayTransactionId,                   
+                   OnePayPaidOn = t.OnePayPaidOn,
+                   TransactionStatus = t.TransactionStatus
+               })
+               .FirstOrDefaultAsync();  // Include related Package
+
+            return transaction;
+        }
+
+
+
+
+    }
+}
---
+++ TaxFiling.Web/Models/OnePayResponseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TaxFiling.Web.Models
+{
+    public class OnePayResponseModel
+    {
+        public OnePayData data { get; set; }
+    }
+
+    public class OnePayData
+    {
+        public string ipg_transaction_id { get; set; }
+        public bool status { get; set; } // SUCCESS / FAILED / PENDING
+        public string paid_on { get; set; }
+        public string transaction_request_datetime { get; set; }
+    }
+}
