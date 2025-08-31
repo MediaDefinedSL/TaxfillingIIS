21b17918f708b6aba6e9d8f53bd62eee5134a250
hsurekaMD <40699662+hsurekaMD@users.noreply.github.com>
2025-08-31 21:08:26 +0530

    commit reset password validation

--- TaxFiling.Web/Models/User/ResetPasswordModel.cs
+++ TaxFiling.Web/Models/User/ResetPasswordModel.cs
@@ -4,13 +4,21 @@ namespace TaxFiling.Web.Models.User
 {
     public class ResetPasswordModel
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public string Token { get; set; }
 
-        [Required, DataType(DataType.Password)]
+        [Required(ErrorMessage = "Please enter a new password")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required, DataType(DataType.Password), Compare("Password", ErrorMessage = "Passwords do not match.")]
+        [Required(ErrorMessage = "Please confirm your new password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; }
     }
+
+    
 }
