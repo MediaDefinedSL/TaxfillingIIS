23120c892bfe2f75c1bc5e47de736cf30b6e28cf
hsurekaMD <40699662+hsurekaMD@users.noreply.github.com>
2025-09-08 21:24:04 +0530

    commit issue fixes

--- TaxFiling.Web/wwwroot/scripts/summary_section.js
+++ TaxFiling.Web/wwwroot/scripts/summary_section.js
@@ -1,26 +1,45 @@
 ﻿$(function () {
 
-    $('#divSummaryAssistedContinue').on('click', function () {  
-    $('#in-this-section-container').show();
-    $.ajax({
-        url: '/SelfOnlineFlow/LoadContactInformation',
-        type: 'GET',
-        success: function (data) {
-            $('#in-this-section-container').html(data);
-            $('.sub-link').removeClass('active');
-            $('#linkContactInformation').addClass('active');
-            $("html, body").animate({ scrollTop: 0 }, "smooth");
-        },
-        error: function () {
-            alert("Error loading section content.");
+    $('#divSummaryAssistedContinue').on('click', function () {
+        var docUploadStatus = $('#docUploadStatus').val();
+        if (docUploadStatus) {
+
+            $('#in-this-section-container').hide();
+            $('#linkSummary').removeClass('active');
+            $('.dropdown-btn').each(function () {
+                var dropdownContent = this.nextElementSibling;
+                if (dropdownContent && dropdownContent.classList.contains("dropdown-container")) {
+                    dropdownContent.style.display = "none";
+                }
+            });
+            showPanel("documents-section");
         }
+        else {
+            $('#in-this-section-container').show();
+            $.ajax({
+                url: '/SelfOnlineFlow/LoadContactInformation',
+                type: 'GET',
+                success: function (data) {
+                    $('#in-this-section-container').html(data);
+                    $('.sub-link').removeClass('active');
+                    $('#linkContactInformation').addClass('active');
+                    $("html, body").animate({ scrollTop: 0 }, "smooth");
+                },
+                error: function () {
+                    alert("Error loading section content.");
+                }
+            });
+        }
+        
+
     });
 
-});
     $('#divSummaryAssistedNext').on('click', function () {
 
         var docUploadStatus = $('#docUploadStatus').val();
         if (docUploadStatus) {
+           
+            $('#in-this-section-container').hide();
             $('#linkSummary').removeClass('active');
             $('.dropdown-btn').each(function () {
                 var dropdownContent = this.nextElementSibling;
@@ -48,25 +67,41 @@
                 }
             });
         }
-   
-        
-       
+
+
+
+
+    });
+    $('#divSummaryAssistedBack').on('click', function () {
+
+        $('#in-this-section-container').show();
+
+        $.ajax({
+            url: '/SelfOnlineFlow/LoadContactInformation',
+            type: 'GET',
+            success: function (data) {
+                $('#in-this-section-container').html(data);
+                $("html, body").animate({ scrollTop: 0 }, "smooth");
+            },
+            error: function () {
+                alert("Error loading section content.");
+            }
+        });
 
     });
 
-$('#linkTaxPayerContinue').on('click', function () {        
-    $('#linkSummary').removeClass('active');    
-    $('.dropdown-btn').each(function () {
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent && dropdownContent.classList.contains("dropdown-container")) {
-            dropdownContent.style.display = "none";
-        }
+    $('#linkTaxPayerContinue').on('click', function () {
+        $('#linkSummary').removeClass('active');
+        $('.dropdown-btn').each(function () {
+            var dropdownContent = this.nextElementSibling;
+            if (dropdownContent && dropdownContent.classList.contains("dropdown-container")) {
+                dropdownContent.style.display = "none";
+            }
+        });
+        showPanel("documents-section");
+
+
+
     });
-    showPanel("documents-section");
-
-   
 
 });
-
-});
-
