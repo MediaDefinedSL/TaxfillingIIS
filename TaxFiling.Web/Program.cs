b590d83fd64b410903bba695e1eb0d39cec3cd4c
Kmalaka-oss <lkodithuwakku@corporate.mediadefined.com>
2025-09-19 16:08:58 +0530

    Update Program.cs

--- TaxFiling.Web/Program.cs
+++ TaxFiling.Web/Program.cs
@@ -14,6 +14,7 @@ using Rotativa.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Configure Serilog
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
